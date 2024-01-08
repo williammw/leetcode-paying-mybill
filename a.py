5b359635f825f575612eea2c437430c5891419a3
wwwwwmw <william.manwai@gmail.com>
2024-01-08 17:16:56 +0800

    Add Fibonacci function to calculate fib_20

--- a.py
+++ a.py
@@ -13,3 +13,19 @@ fib_10
 
 # %%
 # dynamic programming for Fibnacci numbers
+
+
+def fibonacci(n):
+
+    fib = [0] * (n+1)
+    fib[0] = 0
+    if n > 0:
+        fib[1] = 1
+        # from 2 to `n-1`
+    for i in range(2, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+    return fib[n]
+
+
+fib_20 = fibonacci(20)
+fib_20
