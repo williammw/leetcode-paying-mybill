ddf24d669279d9d65dd19f20dfb475bde2ff1579
wwwwwmw <william.manwai@gmail.com>
2024-02-08 10:30:53 +0000

    Refactor code to improve performance and readability

--- leetcode.py
+++ leetcode.py
@@ -65,7 +65,6 @@ ans.val
 
 280 // 10
 # %%
-
 10 % 9
 # %%
 
@@ -130,4 +129,150 @@ a = ''.join(word for word in s if word.isalnum())
 a == a[::-1]
 # O(n), O(n) which n is the length of s
 
+# %%
+s = '234 with word'
+s.isalnum()
+
+''.join(word)
+# %%
+
+
+def myAtoi(s):
+    s = s.strip()
+    if not s:
+        return 0
+    sign = -1 if s[0] == '-' else 1
+    if s[0] in ['-', '+']:
+        s = s[1:]
+    res = 0
+    for i in range(len(s)):
+        if not s[i].isdigit():
+            break
+        res = res * 10 + int(s[i])
+    return max(-2**31, min(sign * res, 2**31-1))
+
+
+# %%
+la = list('11223344abc')
+list.insert(la, 0, 'mas')
+
+
+# %%
+
+nums = [1, 1, 0, 1, 1, 1, 0]
+
+
+def countOne(nums):
+    current_length = 0
+    max_length = 0
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            current_length += 1
+            max_length = max(max_length, current_length)
+        else:
+            current_length = 0
+
+    return max_length
+
+
+x = countOne(nums)
+x
+# %%
+a = 0
+print(a)
+b = 1
+print(b)
+for i in range(20):
+    # complete the code in this loop
+    c = a + b
+    a = b
+    b = c
+    print(c)
+
+# %%
+a = 0
+b = 1
+c = a + b
+
+b = c
+a = c
+
+# %%
+wallet = 8
+
+for price in range(10):
+    if wallet >= price:
+        wallet = wallet - price
+        print("I have", wallet, "left")
+    else:
+        print("I can't afford any more")
+        break
+
+# %%
+wallet = 25
+socks = 0
+
+for price in range(10):
+    if wallet >= price:
+        wallet = wallet - price
+        socks = socks + 1
+    else:
+        break
+
+if (socks % 2 == 0):   # fill in this condition
+    print("I can pair my socks")
+else:
+    print("I need one more...")
+
+# %%
+
+
+number = 3
+steps = 0
+
+for i in range(200):
+    if number == 1:
+        break
+    # fill in the rest of this program
+    elif number % 2 == 0:
+        number = number / 2
+        print(number)
+        steps += 1
+    elif number % 2 == 1:
+        number *= 3
+        number += 1
+        print(number)
+        steps += 1
+
+if number == 1:
+    print("It took", steps, "steps")
+else:
+    print("The number didn't reach 1 yet")
+
+# %%
+for i in range(0, 100, -5):
+    print(i)
+
+# %%
+#  Longest Substring Without Repeating Characters
+
+
+def lengthOfLongestSubstring(s):
+    if not s:
+        return 0
+    max_length = 0
+    start = 0
+    seen = set()
+    for end in range(len(s)):
+        print(seen)
+        while s[end] in seen:
+            # print(seen)
+            seen.remove(s[start])
+            start += 1
+        seen.add(s[end])
+        max_length = max(max_length, end - start + 1)
+    return max_length
+
+
+lengthOfLongestSubstring('abcabcbb')
 # %%
