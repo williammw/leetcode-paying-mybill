671767b63ff7af3ea44b6650a42bc9cd1059b584
wwwwwmw <william.manwai@gmail.com>
2024-04-17 16:20:44 +0100

    Add Dynamic Programming solutions for climbing stairs, minimum cost climbing stairs, tribonacci, and delete and earn problems

--- DynamicProgramming/climbingStairs.py
+++ DynamicProgramming/climbingStairs.py
@@ -36,3 +36,59 @@ def rob(nums):
   for i in range(2, len(nums)):
     dp[i] = max(dp[i-1], dp[i-2] + nums[i])
   return dp[-1]
+
+# %%
+
+arr = [1,2,3,4,6,99]
+print(arr[-1])
+# %%
+
+q = [0] * 99
+q
+
+# %%
+def minCostClimbingStairs(cost):
+  dp = [0] * len(cost)
+  dp[0] = cost[0]
+  dp[1] = cost[1]
+  for i in range(2, len(cost)):
+    dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+  return min(dp[-1], dp[-2])
+
+
+# %%
+for i in range(2, 10):
+  print(i)
+# %%
+def tribonacci(n):
+  dp = [0] * (n+1)
+  dp[0] = 0
+  dp[1] = 1
+  dp[2] = 1
+  for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+  return dp[n]
+
+
+def deleteAndEarn(self, nums):
+    """
+        :type nums: List[int]
+        :rtype: int
+        """
+    if not nums:
+        return 0
+    max_num = max(nums)
+    count = [0] * (max_num + 1)
+    for num in nums:
+        count[num] += num
+    dp = [0] * (max_num + 1)
+    dp[1] = count[1]
+    for i in range(2, max_num + 1):
+        dp[i] = max(dp[i - 1], dp[i - 2] + count[i])
+    return dp[max_num]
+
+
+# %%
+li = [1,2,3,4,5]
+max(li)
+# %%
